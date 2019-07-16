ad8e20a2ee9aba4661d32f3eee2bef601c5e7e35
emrecan-s <36107830+emrecan-s@users.noreply.github.com>
2019-07-16 16:52:41 +0200

    icon-css-

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Appxiety',
-    siteUrl: 'https://appxiety-web1.herokuapp.com/
+    siteUrl: 'https://needstobetheproductionurl.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
