1b7740a13885f0e2269c5f157cefc7f05f900ecf
emrecan-s <36107830+emrecan-s@users.noreply.github.com>
2019-07-16 16:48:37 +0200

    icon-css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Appxiety',
-    siteUrl: 'https://needstobetheproductionurl.com'
+    siteUrl: 'https://appxiety-web1.herokuapp.com/
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -14,7 +14,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/appxiety_icon.ico', // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-remark`,
